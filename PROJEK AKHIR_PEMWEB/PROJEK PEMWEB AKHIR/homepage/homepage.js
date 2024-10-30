eb9170bd17a619b1a7210d7bcb960e7a1249e081
Fathurrahman Nasution <fathur.6913@gmail.com>
2024-10-30 23:46:46 +0700

    footer

--- PROJEK AKHIR_PEMWEB/PROJEK PEMWEB AKHIR/homepage/homepage.js
+++ PROJEK AKHIR_PEMWEB/PROJEK PEMWEB AKHIR/homepage/homepage.js
@@ -1,5 +1,5 @@
-fetch('/footer.html')
-.then(response => response.text())
-.then(data => {
-  document.body.insertAdjacentHTML('beforeend', data);
-});
+fetch('/PROJEK AKHIR_PEMWEB/PROJEK PEMWEB AKHIR/footer/footer.html')
+      .then(response => response.text())
+      .then(data => {
+        document.body.insertAdjacentHTML('beforeend', data);
+      });
