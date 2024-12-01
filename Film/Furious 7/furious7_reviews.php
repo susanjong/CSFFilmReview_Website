599a0a2c0f07ae9637baec34cacad34bdc024862
Ferarineyesh <changferarine@gmail.com>
2024-12-01 23:44:55 +0700

    update css furious 7

--- Film/Furious 7/furious7_reviews.php
+++ Film/Furious 7/furious7_reviews.php
@@ -65,7 +65,7 @@ try {
     <title>Tampilan Review</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="/Film/The Conjuring/conjuring.css">
+    <link rel="stylesheet" href="/Film/Furious 7/furious7.css">
 </head>
 
 <body>
